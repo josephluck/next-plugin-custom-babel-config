d72c0822271dcb1eb930ca9847a49b7095798c92
rutzfutz <rutzfutz@users.noreply.github.com>
2021-08-21 15:55:53 +0200

    Add Next.js 11 support, update tests

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const path = require('path');
 
+function isBabelLoader (loader) {
+  return loader && (
+    loader === 'next-babel-loader' ||
+	// Next.js 11 uses an absolute path to its loader
+    loader.replace(/\\/g, '/').match('/next/dist/build/babel/loader/index.js$')
+  )
+}
+
 module.exports = function (nextConfig) {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
@@ -9,11 +17,11 @@ module.exports = function (nextConfig) {
       config.module.rules.forEach((rule) => {
         if (rule.use) {
           if (Array.isArray(rule.use)) {
-            const babelLoader = rule.use.find(use => typeof use === 'object' && use.loader === 'next-babel-loader');
+            const babelLoader = rule.use.find(use => typeof use === 'object' && isBabelLoader(use.loader));
             if (babelLoader && babelLoader.options) {
               babelLoader.options.configFile = nextConfig.babelConfigFile;
             }
-          } else if (rule.use.loader === 'next-babel-loader') {
+          } else if (isBabelLoader(rule.use.loader)) {
             rule.use.options.configFile = nextConfig.babelConfigFile;
           }
         }
